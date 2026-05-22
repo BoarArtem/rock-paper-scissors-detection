646fe6ead4548b8ae697bf7911f898cc613f795d
BoarArtem <workboar@gmail.com>
2026-05-22 15:15:10 +0300

    implementing dataset

---
+++ inference.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("runs/detect/train-2/weights/best.pt")
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+
+    results = model(frame, imgsz=320, conf=0.1)
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("Detection", annotated_frame)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
